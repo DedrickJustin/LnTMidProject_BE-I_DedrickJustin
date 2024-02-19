b45cafcf5e652a62b594c7db3c2df15cb2a55d0b
DedrickJustin <dedrickjustin057@gmail.com>
2024-02-19 18:50:40 +0700

    First Commit

---
+++ example-app/resources/views/home.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WB PT Chipichapa | Home</title>
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+</head>
+<body>
+<div class="wrap">
+		
+        <!-- HEADER -->
+        <div class="header"></div>
+        
+        <!-- MENU -->
+        <div class="topnav">
+            <a class="active" href="#home">Home</a>
+            <a href="#MembersInformation">Members Information</a>
+            <a href="#Register">Register</a>
+        </div>
+            
+            
+            <div class="content">
+                <br/>
+                
+                <h2 style="font-size:14px;">Selamat datang di website resmi PT. Chipichapa</h2>
+                <p style="text-align:justify">PT ChipiChapa merupakan software house yang terkemuka di masyarakat. Dengan kualitas produk yang dibuat oleh PT ChipiChapa, perusahaan ini biasanya menerima 600.000 proyek per tahun.</p>
+                
+            
+            </div><!-- END CONTENT -->
+            <div class="clearfix"></div>
+            
+        </div><!-- END MAIN -->
+        
+        <!-- FOOTER -->
+        <div class="footer">
+            PT. Chipichapa
+        </div>
+        
+    </div><!-- END WRAP -->
+</body>
+</html>
